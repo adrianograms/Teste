1368dc2900353d4938c5c8fdbc6164ae30e8b0a0
Adriano Grams <adrianograms@hotmail.com>
2018-12-31 16:15:33 -0200

    Criando operacao de instanciacao

--- src/main/java/com/adriano/crusomc/CursomcApplication.java
+++ src/main/java/com/adriano/crusomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.adriano.crusomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.adriano.crusomc.domain.Categoria;
+import com.adriano.crusomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
