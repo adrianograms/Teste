b9e46ce89ebded21cb0f17c1eb90599a9e1edd2a
Adriano Grams <adrianograms@hotmail.com>
2019-01-01 14:37:05 -0200

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/adriano/crusomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.adriano.crusomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adriano.crusomc.domain.Pedido;
+import com.adriano.crusomc.repositories.PedidoRepository;
+import com.adriano.crusomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		
+	}
+}
